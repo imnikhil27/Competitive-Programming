a90e07d2f0e7bc6a58ae54f74b8f531105d06b56
Nikhil Moghe <nikhilmoghe1998@gmail.com>
2019-04-13 13:39:34 +0530

    Update template.cpp
    updated template

--- template.cpp
+++ template.cpp
@@ -1,9 +1,14 @@
 #include <bits/stdc++.h>
 #define SEIVE_SIZE 1000000
-#define ll long long
+#define ll long long int
 #define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
 #define mod 1000000007
 #define inf 999999999999999999
+#define pb push_back
+#define test cin>>t; for(int test_num=1;test_num<=t;test_num++)
+#define input_loop int n;cin>>n;vector<int>arr(n);for(int i=0;i<n;i++)cin>>arr[i]
+#define output_loop for(int i=0;i<n;i++)cout<<arr[i]<<" ";cout<<endl
+#define mid(s,e) (s+(e-s)/2)
 
 using namespace std;
 
